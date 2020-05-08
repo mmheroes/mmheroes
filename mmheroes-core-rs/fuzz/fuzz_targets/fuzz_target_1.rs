8974919f02bc5299d0a6e8601855a9fceea2e142
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2020-05-09 01:06:30 +0300

    Update fuzzing target for the new API

--- mmheroes-core-rs/fuzz/fuzz_targets/fuzz_target_1.rs
+++ mmheroes-core-rs/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -2,7 +2,7 @@
 use libfuzzer_sys::fuzz_target;
 
 use mmheroes_core::logic::{Game, GameMode};
-use mmheroes_core::ui::{recorded_input::RecordedInputRenderer, GameUI};
+use mmheroes_core::ui::GameUI;
 
 fuzz_target!(|input: &[u8]| {
     let structured_input = input.iter().map(|b| {
@@ -11,12 +11,15 @@ fuzz_target!(|input: &[u8]| {
             0   => KeyUp,
             1   => KeyDown,
             2   => Enter,
-            255 => EOF,
             _   => Other,
         }
     });
-    let mut renderer = RecordedInputRenderer::new(structured_input);
-    let game = Game::new(GameMode::God, 0);
-    let mut runner = GameUI::new(&mut renderer, game);
-    let _result = runner.run();
+    let mut game = Game::new(GameMode::God, 0);
+    let mut game_ui = GameUI::new(&mut game);
+
+    for input_value in structured_input {
+        if !game_ui.continue_game(input_value) {
+            break;
+        }
+    }
 });
