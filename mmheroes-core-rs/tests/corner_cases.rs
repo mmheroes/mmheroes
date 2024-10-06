551789f136268bcd0a0bbc05d36fb36c679532d7
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2024-10-06 17:30:27 +0200

    Use assert_matches! in a test

--- mmheroes-core-rs/tests/corner_cases.rs
+++ mmheroes-core-rs/tests/corner_cases.rs
@@ -27,20 +27,17 @@ fn overstudy_to_zero_health() {
 fn study_with_negative_brain_level() {
     initialize_game!((1641336778475, GameMode::Normal) => state, game_ui);
     replay_game(&mut game_ui, "3r2↓r2↓r4↓r2↑2r4↓r3↑r↓2r3↑r↓2r4↓r↓2r3↑r↑2r3↑r↑2r3↑r↑2r3↑2r3↑2r2↑2r3↑2r3↑2r2↑r↓3r2↓2r");
-    match state.borrow().screen() {
-        GameScreen::Study(state) => {
-            assert_eq!(state.player().brain(), BrainLevel(-1));
-            assert_eq!(
-                state
-                    .player()
-                    .status_for_subject(Subject::AlgebraAndNumberTheory)
-                    .knowledge(),
-                BrainLevel(5)
-            );
-            assert_eq!(state.current_time(), Time(15));
-        }
-        _ => panic!("Unexpected screen"),
-    }
+    assert_matches!(state.borrow().screen(), GameScreen::Study(state) => {
+        assert_eq!(state.player().brain(), BrainLevel(-1));
+        assert_eq!(
+            state
+                .player()
+                .status_for_subject(Subject::AlgebraAndNumberTheory)
+                .knowledge(),
+            BrainLevel(5)
+        );
+        assert_eq!(state.current_time(), Time(15));
+    });
     replay_game(&mut game_ui, "r");
     let borrowed_state = state.borrow();
     match borrowed_state.screen() {
