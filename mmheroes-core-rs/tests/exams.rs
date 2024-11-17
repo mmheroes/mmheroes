9376276ada793aab181e85f08f2a6b79f1149b3d
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2024-11-17 16:18:53 +0100

    Use visualization in exam tests

--- mmheroes-core-rs/tests/exams.rs
+++ mmheroes-core-rs/tests/exams.rs
@@ -1,9 +1,8 @@
 mod common;
 
-use assert_matches::assert_matches;
 use common::*;
 use mmheroes_core::logic::actions::PlayStyle;
-use mmheroes_core::logic::{Action, GameMode, GameScreen, Subject};
+use mmheroes_core::logic::GameMode;
 
 #[test]
 fn exam_list_in_punk() {
@@ -15,13 +14,20 @@ fn exam_list_in_punk() {
     // - Алгебра 13:00–15:00
     // - English 14:00-16:00
     replay_game(game_ui, "4↓2r");
-    assert_matches!(
-        state.observable_state().screen(),
-        GameScreen::GoToProfessor(_)
-    );
-    assert_eq!(
-        state.observable_state().available_actions(),
-        [Action::DontGoToProfessor]
+    assert_ui!(
+        game_ui,
+        "
+Сегодня 22е мая; 8:00    Версия gamma3.14   Алгебра и Т.Ч.        2   Плохо
+Самочувствие: отличное (41)                 Мат. Анализ           0   Плохо
+Финансы: Надо получить деньги за май...     Геометрия и Топология 3   Плохо
+Голова свежая (5)                           Информатика           0   Плохо
+Немного устал (4)                           English               4   Плохо
+У тебя много друзей (5)                     Физ-ра                0   Плохо
+
+Ты сейчас на факультете. К кому идти?
+
+Ни к кому▁
+"
     );
 
     // Идём в общагу и отдыхаем до 13:00
@@ -29,16 +35,21 @@ fn exam_list_in_punk() {
 
     // Снова идём на факультет.
     replay_game(game_ui, "4↓2r");
-    assert_matches!(
-        state.observable_state().screen(),
-        GameScreen::GoToProfessor(_)
-    );
-    assert_eq!(
-        state.observable_state().available_actions(),
-        [
-            Action::Exam(Subject::AlgebraAndNumberTheory),
-            Action::DontGoToProfessor
-        ]
+    assert_ui!(
+        game_ui,
+        "
+Сегодня 22е мая; 13:00   Версия gamma3.14   Алгебра и Т.Ч.        2   Плохо
+Самочувствие: отличное (96)                 Мат. Анализ           0   Плохо
+Финансы: Надо получить деньги за май...     Геометрия и Топология 3   Плохо
+Голова свежая (5)                           Информатика           0   Плохо
+Немного устал (4)                           English               4   Плохо
+У тебя много друзей (5)                     Физ-ра                0   Плохо
+
+Ты сейчас на факультете. К кому идти?
+
+Всемирнов М.А.▁
+Ни к кому
+"
     );
 
     // Идём в общагу и отдыхаем до 14:00
@@ -46,17 +57,22 @@ fn exam_list_in_punk() {
 
     // Снова идём на факультет.
     replay_game(game_ui, "4↓2r");
-    assert_matches!(
-        state.observable_state().screen(),
-        GameScreen::GoToProfessor(_)
-    );
-    assert_eq!(
-        state.observable_state().available_actions(),
-        [
-            Action::Exam(Subject::AlgebraAndNumberTheory),
-            Action::Exam(Subject::English),
-            Action::DontGoToProfessor
-        ]
+    assert_ui!(
+        game_ui,
+        "
+Сегодня 22е мая; 14:00   Версия gamma3.14   Алгебра и Т.Ч.        2   Плохо
+Самочувствие: отличное (106)                Мат. Анализ           0   Плохо
+Финансы: Надо получить деньги за май...     Геометрия и Топология 3   Плохо
+Голова свежая (5)                           Информатика           0   Плохо
+Немного устал (4)                           English               4   Плохо
+У тебя много друзей (5)                     Физ-ра                0   Плохо
+
+Ты сейчас на факультете. К кому идти?
+
+Всемирнов М.А.▁
+Влащенко Н.П.
+Ни к кому
+"
     );
 
     // Идём в общагу и отдыхаем до 15:00
@@ -64,13 +80,21 @@ fn exam_list_in_punk() {
 
     // Снова идём на факультет.
     replay_game(game_ui, "4↓2r");
-    assert_matches!(
-        state.observable_state().screen(),
-        GameScreen::GoToProfessor(_)
-    );
-    assert_eq!(
-        state.observable_state().available_actions(),
-        [Action::Exam(Subject::English), Action::DontGoToProfessor]
+    assert_ui!(
+        game_ui,
+        "
+Сегодня 22е мая; 15:00   Версия gamma3.14   Алгебра и Т.Ч.        2   Плохо
+Самочувствие: отличное (116)                Мат. Анализ           0   Плохо
+Финансы: Надо получить деньги за май...     Геометрия и Топология 3   Плохо
+Голова свежая (5)                           Информатика           0   Плохо
+Немного устал (4)                           English               4   Плохо
+У тебя много друзей (5)                     Физ-ра                0   Плохо
+
+Ты сейчас на факультете. К кому идти?
+
+Влащенко Н.П.▁
+Ни к кому
+"
     );
 
     // Идём в общагу и отдыхаем до 16:00
@@ -78,12 +102,19 @@ fn exam_list_in_punk() {
 
     // Снова идём на факультет.
     replay_game(game_ui, "4↓2r");
-    assert_matches!(
-        state.observable_state().screen(),
-        GameScreen::GoToProfessor(_)
-    );
-    assert_eq!(
-        state.observable_state().available_actions(),
-        [Action::DontGoToProfessor]
+    assert_ui!(
+        game_ui,
+        "
+Сегодня 22е мая; 16:00   Версия gamma3.14   Алгебра и Т.Ч.        2   Плохо
+Самочувствие: отличное (124)                Мат. Анализ           0   Плохо
+Финансы: Надо получить деньги за май...     Геометрия и Топология 3   Плохо
+Голова свежая (5)                           Информатика           0   Плохо
+Немного устал (4)                           English               4   Плохо
+У тебя много друзей (5)                     Физ-ра                0   Плохо
+
+Ты сейчас на факультете. К кому идти?
+
+Ни к кому▁
+"
     );
 }
